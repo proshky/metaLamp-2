c5f39d5e8ca24ea19c88703ee9eff17582dd3d3c
proshka <proshka333@gmail.com>
2021-06-02 15:53:37 +0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,115 @@
+const path = require("path")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const CopyWebPackPlugin = require("copy-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin")
+const TerserWebpackPlugin = require("terser-webpack-plugin")
+
+const isDev = process.env.NODE_ENV === "development"
+const isProd = !isDev
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: "all"
+        }
+    }
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`
+
+const cssLoaders = (extra) => {
+    const loaders = [
+        {
+            loader: MiniCssExtractPlugin.loader,
+        }, "css-loader"
+    ]
+    if (extra) {
+        loaders.push(extra)
+    }
+    return loaders
+}
+
+module.exports = {
+    context: path.resolve(__dirname, "src"),
+    mode: "development",
+    entry: {
+        main: ["@babel/polyfill", "./index.js"]
+    },
+    output: {
+        filename: filename("js"),
+        path: path.resolve(__dirname, "dist")
+    },
+    resolve: {
+        extensions: [".js"],
+        alias: {
+            "@": path.resolve(__dirname, "src")
+        }
+    },
+    optimization: optimization(),
+    devServer: {
+        port: 3007,
+        open: true,
+    },
+    devtool: isDev ? "source-map" : false,
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./index.pug"
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebPackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src/images"),
+                    to: path.resolve(__dirname, "dist")
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename("css"),
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders("sass-loader")
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env"
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.pug$/,
+                use: [
+                    {
+                        loader: "pug-loader",
+                        options: {
+                            pretty: isDev
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}

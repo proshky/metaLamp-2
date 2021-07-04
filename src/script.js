3752738fb9368f943ab7a86bb360c8aa1aa910b8
proshka <proshka333@gmail.com>
2021-07-04 07:05:20 +0500

    refactoring dropdown js+styles

--- src/script.js
+++ src/script.js
@@ -1,49 +1,75 @@
-const myFunction = () => {
-    document.getElementById("myDropdown").classList.toggle("show");
-};
+const btnMenu = document.querySelector(".dropdown")
+const menu = document.querySelector(".dropdown__dropdownContent")
+const inputMenu = document.querySelector(".dropdown__dropdownInput")
+const toggleMenu = () => {
+    menu.classList.toggle("show")
+    inputMenu.classList.toggle("open")
+}
 
-/*window.onclick = event => {
-    if (!event.target.matches(".dropdown__dropdownBtn")) {
+btnMenu.addEventListener("click", e => {
+    e.stopPropagation()
+    toggleMenu();
+})
 
-        let dropdowns = document.getElementsByClassName("dropdown__dropdownContent");
-        let i;
-        for (i = 0; i < dropdowns.length; i++) {
-            let openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+menu.addEventListener("click", e => {
+    if (e.target.className === "switcher__btnPlus") {
+        e.target.parentElement.children[1].innerText = Number(e.target.parentElement.children[1].innerText) + 1
+        e.target.parentElement.children[0].removeAttribute("disabled")
+        if (!document.querySelectorAll(".dropdown__clear")[0].classList.contains("dropdown__clear_show")){
+            document.querySelectorAll(".dropdown__clear")[0].classList.toggle("dropdown__clear_show")
+        }
+    }
+    if (e.target.className === "switcher__btnMinus") {
+        e.target.parentElement.children[1].innerText = Number(e.target.parentElement.children[1].innerText) - 1
+        if (e.target.parentElement.children[1].innerText === "0") {
+            e.target.parentElement.children[0].setAttribute("disabled", "disabled")
+            let array = []
+            document.querySelectorAll(".switcher__value").forEach(a => array.push(a.innerText))
+            if (array.filter(a=> a === "0").length === 3){
+                if (document.querySelectorAll(".dropdown__dropdownInput")[0].defaultValue === "Сколько гостей") {
+                document.querySelectorAll(".dropdown__clear")[0].classList.toggle("dropdown__clear_show")
+                }
             }
         }
     }
-}*/
-
-const selectDropFunction = (value) => {
-    document.getElementsByClassName("dropdown__dropdownInput")[0].value = value
-}
-
-const turnOffDisableMinus = (className) => {
-    document.querySelectorAll(`.${className}`)[0].children[1].children[0].removeAttribute("disabled");
-}
-
-const turnOnDisableMinus = (className) => {
-    document.querySelectorAll(`.${className}`)[0].children[1].children[0].setAttribute("disabled", "disabled");
-}
-
-const functionPlus = (className) => {
-    document.getElementsByClassName(className)[0].children[1].children[1].innerText = Number(document.getElementsByClassName(className)[0].children[1].children[1].innerText) + 1
-    turnOffDisableMinus(className)
-}
-const functionMinus = (className) => {
-    document.getElementsByClassName(className)[0].children[1].children[1].innerText = Number(document.getElementsByClassName(className)[0].children[1].children[1].innerText) - 1
-    if (document.getElementsByClassName(className)[0].children[1].children[1].innerText==="0"){
-        turnOnDisableMinus(className)
+    if (e.target.classList.contains("dropdown__clear")) {
+        document.querySelectorAll(".switcher__value").forEach(a => a.innerText = 0)
+        document.getElementsByClassName(`dropdown__dropdownInput`)[0].defaultValue = "Сколько гостей"
+        document.querySelectorAll(`.switcher__btnMinus`).forEach(a => a.setAttribute("disabled", "disabled"))
+        document.querySelectorAll(".dropdown__clear")[0].classList.toggle("dropdown__clear_show")
     }
-}
+    if (e.target.className === "dropdown__toApply"){
+        let array = []
+        document.querySelectorAll(".switcher__value").forEach(a => array.push(a.innerText))
+        let arrayGuests = [Number(array[0]) + Number(array[1]), Number(array[2])]
+        let textValueGuests = declOfNum(arrayGuests[0], ["гость", "гостя", "гостей"])
+        let textValueToddlers = declOfNum(arrayGuests[1], ["младенец", "младенца", "младенцев"])
+        let newValue
+        if (arrayGuests[1] !== 0) {
+            newValue = `${arrayGuests[0]} ${textValueGuests}, ${arrayGuests[1]} ${textValueToddlers}`
+        } else {
+            newValue = `${arrayGuests[0]} ${textValueGuests}`
+        }
+        if (arrayGuests[0] === 0) {
+            newValue = "Сколько гостей"
+        }
+        document.getElementsByClassName(`dropdown__dropdownInput`)[0].defaultValue = newValue
+        toggleMenu()
+    }
+    e.stopPropagation()
+    return
+})
 
-const clearValue = () => {
-    document.querySelectorAll('.switcher__value').forEach(a => a.innerText = 0)
-    document.getElementsByClassName(`dropdown__dropdownInput`)[0].defaultValue = "Сколько гостей"
-    document.querySelectorAll(`.switcher__btnMinus`).forEach(a =>  a.setAttribute("disabled", "disabled"))
-}
+
+/*document.addEventListener('click', e => {
+    const target = e.target
+    const its_menu = target == menu || menu.contains(target)
+    const its_btnMenu = target == btnMenu
+    const menu_is_active = menu.classList.contains('show')
+    if (!its_menu && !its_btnMenu && menu_is_active) {
+        toggleMenu()
+    }
+})*/
 
 function declOfNum(n, text_forms) {
     n = Math.abs(n) % 100;
@@ -60,21 +86,5 @@ function declOfNum(n, text_forms) {
     return text_forms[2];
 }
 
-const toApply = () => {
-    let array = []
-    document.querySelectorAll(".switcher__value").forEach(a => array.push(a.innerText))
-    let arrayGuests = [Number(array[0]) + Number(array[1]), Number(array[2])]
-    let textValueGuests = declOfNum(arrayGuests[0], ["гость", "гостя", "гостей"])
-    let textValueToddlers = declOfNum(arrayGuests[1], ["младенец", "младенца", "младенцев"])
-    let newValue
-    if (arrayGuests[1] !== 0) {
-       newValue = `${arrayGuests[0]} ${textValueGuests}, ${arrayGuests[1]} ${textValueToddlers}`
-    } else {
-        newValue = `${arrayGuests[0]} ${textValueGuests}`
-    }
-    if(arrayGuests[0] === 0){
-        newValue = "Сколько гостей"
-    }
-    document.getElementsByClassName(`dropdown__dropdownInput`)[0].defaultValue = newValue
-}
+
 
